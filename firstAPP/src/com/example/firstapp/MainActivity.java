925d9e9de56955346c07d47287a930ed460e4317
wushang1987 <wushang1987@gmail.com>
2014-02-23 20:22:13 +0800

    add phonegap & jquery mobile

--- firstAPP/src/com/example/firstapp/MainActivity.java
+++ firstAPP/src/com/example/firstapp/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.firstapp;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import org.apache.cordova.DroidGap;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	//	setContentView(R.layout.activity_main);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
 }
